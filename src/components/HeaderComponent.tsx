192a567b03c02164bd832c96c987777b6000d1da
mpshos <mps9vf@gmail.com>
2020-09-14 20:31:03 -0600

    Remove default content. Add beginnings of header

---
+++ src/components/HeaderComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Jumbotron } from 'reactstrap';
+
+export const Header = () => {
+
+    return (
+        <Jumbotron className="bg-primary text-white text-center">
+            <h1 className="display-3">Hello</h1>
+        </Jumbotron>
+    );
+}
