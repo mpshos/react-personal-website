33329a99271d78e13042ff45d4313657dfedf6f7
mpshos <mps9vf@gmail.com>
2020-10-20 16:12:19 -0600

    WIP - updating experience

--- src/components/ExperienceComponent.tsx
+++ src/components/ExperienceComponent.tsx
@@ -15,7 +15,22 @@ function Experience() {
             </div>
             <div className="row">
                 <div className="col-12">
-                    <h4 class="text-center">Garmin</h4>
+                    <h4>Garmin</h4>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12">
+                    <h6>Description</h6>
+                    <p>I worked at Garmin for a total of about 5 and a half years including my summer internship before my full time position. Aviation electronics (avionics) is the segment that I was involved with. My team was one of the many that made up the Aviation Displays UI group. We were tasked with creating the UI that was presented to the pilot.</p>
+                </div>
+                <div className="col-12">
+                    <h6>Major Contributions</h6>
+                    <p>After being promoted to Software Engineer 2, I was assigned as the airframe lead to the Embraer Phenom line of executive jets. The airframe lead has many responsibilities, but at its core, you're the engineer responsible for making sure that particular customer's needs are being met. This includes:</p>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item">Discussing new features with client engineering teams to determine requirements.</li>
+                        <li className="list-group-item">Discussing new features with client engineering teams to determine requirements.</li>
+                        <li className="list-group-item">Discussing new features with client engineering teams to determine requirements.</li>
+                    </ul>
                 </div>
             </div>
         </div>
