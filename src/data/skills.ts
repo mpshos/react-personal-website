2a2d08db872eb50d85ac11949e9f5ccfcf6eee43
mpshos <mps9vf@gmail.com>
2020-10-21 15:09:55 -0600

    Added to skills list

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -18,5 +18,41 @@ export const SKILLS: Skill[] = [
         image: 'assets/images/java-original.svg',
         text: 'Java was my introduction to object oriented programming. I used it a bunch at school and in my first internship. I am a bit out of practice at this point, but am comfortable with it and could pick it back up quickly. I\'ve also used it when playing around with Android development',
         experience: '2 years'
+    },
+    {
+        name: 'React',
+        image: 'assets/images/react-original.svg',
+        text: 'I dipped my toes into the React world towards the end of my career at Garmin. I continued learning more about React after leaving that position. I have really enjoyed working with React. I used it to build this simple site, and plan on continue using it for my side projects.',
+        experience: '6 Months'
+    },
+    {
+        name: 'Javascript',
+        image: 'assets/images/javascript-original.svg',
+        text: `I've used Javascript in school and all my professional roles. At Garmin I used vanilla javascript quite a bit as that is all our simplified embedded browser supported. More recently I've been working with React and using more modern versions of Javascript and even Typescript.`,
+        experience: '4 years'
+    },
+    {
+        name: 'Typescript',
+        image: 'assets/images/typescript-original.svg',
+        text: 'I have been using Typescript with the React projects that I\'ve been working on recently.',
+        experience: '6 months'
+    },
+    {
+        name: 'Bootstrap',
+        image: 'assets/images/bootstrap-plain.svg',
+        text: 'I\'ve been using Bootstrap 4 for the recent web projects I\'ve been working on.',
+        experience: '6 months'
+    },
+    {
+        name: 'MySQL',
+        image: 'assets/images/mysql-original.svg',
+        text: 'DBMS wise I have worked with MySQL the most. Not an expert, but I can use it effectively.',
+        experience: '1 year'
+    },
+    {
+        name: 'MongoDB',
+        image: 'assets/images/mongodb-original.svg',
+        text: 'I was recently introduced to MongoDB and NoSQL database usage in general. I have enjoyed it so far and plan to use it in an upcoming project.',
+        experience: '2 Months'
     }
 ];
